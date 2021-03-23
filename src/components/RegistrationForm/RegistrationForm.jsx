066e5c4aecc15ef75140c7b185743f9b6ea604be
Vadim Semenov <gosusty13@gmail.com>
2021-03-24 01:43:17 +0200

    update form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import TextareaAutosize from 'react-textarea-autosize';
 import './RegistrationForm.css'
 
+const defaultValues = {
+    Name: "",
+    About: "",
+};
+
 const RegistrationForm = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
+    const { register, handleSubmit, watch, errors, control } = useForm({
+        defaultValues,
+        // mode: 'onBlur',
+        mode: 'onSubmit',
+        shouldFocusError: true,
+    });
     const onSubmit = data => console.log(data);
 
     return (
@@ -11,13 +22,49 @@ const RegistrationForm = () => {
             className="registration-form__container"
             onSubmit={handleSubmit(onSubmit)}
         >
-            {/* register your input into the hook by invoking the "register" function */}
-            <input name="example" defaultValue="test" ref={register} />
+            <section className="registration-form__section">
+                <label
+                    htmlFor="Name"
+                    className="registration-form__label"
+                >
+                    Имя
+                </label>
+                <input
+                    name="Name"
+                    // placeholder="Введите Ваше имя"
+                    className={`registration-form__input${errors.Name ? '-error' : ''}`}
+                    ref={register({ required: true, minLength: 3 })}
+                />
+                <div className="registration-form__error-validation">
+                    {errors.Name?.type === "required" && 'Это поле обязательно для заполнения'}
+                    {errors.Name?.type === "minLength" && 'Минимум 3 символа'}
+                </div>
+            </section>
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input name="exampleRequired" ref={register({ required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+            <section className="registration-form__section">
+                <label
+                    htmlFor="About"
+                    className="registration-form__label"
+                >
+                    Ваше хобби
+                </label>
+                <Controller
+                    as={
+                        <TextareaAutosize
+                            style={{padding: '10px 15px', fontSize: '16px', borderRadius: '4px'}}
+                        />
+                    }
+                    name="About"
+                    minRows={3}
+                    maxRows={6}
+                    control={control}
+                    ref={register({ required: true, minLength: 10 })}
+                />
+                <div className="registration-form__error-validation">
+                    {errors.About?.type === "required" && 'Это поле обязательно для заполнения'}
+                    {errors.About?.type === "minLength" && 'Минимум 10 символа'}
+                </div>
+            </section>
 
             <input type="submit" />
         </form>

b245467aca032125f0d2cdc5668a4a73aae7a0fa
Vadim Semenov <gosusty13@gmail.com>
2021-03-31 00:43:05 +0300

    added flag-select

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useForm, Controller } from "react-hook-form";
 import TextareaAutosize from 'react-textarea-autosize';
-import Select from 'react-select-native';
+import Select from 'react-select';
+import countryToFlag from '../../utils/countryToFlag'
+import { countries, englishLevel } from '../../config'
 import './RegistrationForm.css'
 
 const defaultValues = {
     name: "",
     about: "",
-    age: 17,
+    age: { value: 17, label: 17 },
+    country: { value: "RU", label: "Russian Federation", phone: "7" },
+    english_level: { value: 'middle', label: 'Средний'},
 };
+
+const formatOptionLabel = ({ value, label }) => (
+    <div style={{ display: "flex" }}>
+        <div>{countryToFlag(value)}</div>
+        <div style={{ marginLeft: "10px" }}>{label}</div>
+    </div>
+);
 
 const RegistrationForm = () => {
     const { register, handleSubmit, errors, clearErrors, control } = useForm({
@@ -45,28 +56,50 @@ const RegistrationForm = () => {
                 </div>
             </section>
 
+            <section className="registration-form__section-double">
+                <section className="registration-form__section" style={{marginRight: '10px'}}>
+                    <label
+                        htmlFor="age"
+                        className="registration-form__label"
+                    >
+                        Возраст
+                    </label>
+                    <Controller
+                        name="age"
+                        as={Select}
+                        options={Array.from({length: 45}, (v, i) => ({value: 17+i, label: 17+i}))}
+                        control={control}
+                    />
+                </section>
+
+                <section className="registration-form__section">
+                    <label
+                        htmlFor="country"
+                        className="registration-form__label"
+                    >
+                        Страна проживания
+                    </label>
+                    <Controller
+                        name="country"
+                        as={Select}
+                        formatOptionLabel={formatOptionLabel}
+                        options={countries}
+                        control={control}
+                    />
+                </section>
+            </section>
+
             <section className="registration-form__section">
                 <label
-                    htmlFor="age"
+                    htmlFor="english_level"
                     className="registration-form__label"
                 >
-                    Ваш возраст
+                    Уровень владения английским языком
                 </label>
                 <Controller
-                    render={(
-                        { onChange, value, ref }
-                    ) => (
-                        <Select
-                            onChange={e => onChange(e)}
-                            options={Array.from({length: 45}, (v, i) => ({value: 17+i, label: 17+i}))}
-                            unselected={{value: 17, label: 17}}
-                            value={value}
-                            inputRef={ref}
-                            className='registration-form__select'
-                            open={true}
-                        />
-                    )}
-                    name="age"
+                    name="english_level"
+                    as={Select}
+                    options={englishLevel}
                     control={control}
                 />
             </section>
---
+++ src/utils/countryToFlag.js
@@ -0,0 +1,11 @@
+const countryToFlag = (isoCode) => {
+    return typeof String.fromCodePoint !== "undefined"
+        ? isoCode
+            .toUpperCase()
+            .replace(/./g, char =>
+                String.fromCodePoint(char.charCodeAt(0) + 127397)
+            )
+        : isoCode;
+}
+
+export default countryToFlag;

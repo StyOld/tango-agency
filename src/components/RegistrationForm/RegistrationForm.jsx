c7da2957568ec758fec102af1348b093036ba205
Vadim Semenov <gosusty13@gmail.com>
2021-04-01 01:17:23 +0300

    update form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -8,11 +8,12 @@ import './RegistrationForm.css'
 
 const defaultValues = {
     name: "",
-    about: "",
     age: { value: 17, label: 17 },
     country: { value: "RU", label: "Russian Federation", phone: "7" },
     english_level: { value: 'middle', label: 'Средний'},
     additional_lang: "",
+    about: "",
+    phone: "7",
 };
 
 const formatOptionLabel = ({ value, label }) => (
@@ -53,12 +54,12 @@ const RegistrationForm = () => {
                 />
                 <div className="registration-form__error-validation">
                     {errors.name?.type === "required" && 'Это поле обязательно для заполнения'}
-                    {errors.name?.type === "minLength" && 'Минимум 3 символа'}
+                    {errors.name?.type === "minLength" && 'Минимум 3 буквы'}
                 </div>
             </section>
 
-            <section className="registration-form__section-double">
-                <section className="registration-form__section" style={{marginRight: '10px'}}>
+            <div className="registration-form__section-double" style={{ marginBottom: '13px' }}>
+                <section className="registration-form__section" style={{ flexGrow: 1, marginRight: 10}}>
                     <label
                         htmlFor="age"
                         className="registration-form__label"
@@ -70,10 +71,11 @@ const RegistrationForm = () => {
                         as={Select}
                         options={Array.from({length: 45}, (v, i) => ({value: 17+i, label: 17+i}))}
                         control={control}
+                        menuColor='red'
                     />
                 </section>
 
-                <section className="registration-form__section">
+                <section className="registration-form__section" style={{flexGrow: 2}}>
                     <label
                         htmlFor="country"
                         className="registration-form__label"
@@ -88,9 +90,9 @@ const RegistrationForm = () => {
                         control={control}
                     />
                 </section>
-            </section>
+            </div>
 
-            <section className="registration-form__section">
+            <section className="registration-form__section" style={{marginBottom: '27px'}}>
                 <label
                     htmlFor="english_level"
                     className="registration-form__label"
@@ -118,12 +120,17 @@ const RegistrationForm = () => {
                             minRows={2}
                             maxRows={2}
                             placeholder="Напишите другие языки и уровень их владения"
-                            className="registration-form__textarea"
+                            className={`registration-form__textarea${errors.additional_lang ? '-error' : ''}`}
+                            onClick={() => clearErrors('additional_lang')}
                         />
                     }
                     name="additional_lang"
                     control={control}
+                    rules={{ required: true }}
                 />
+                <div className="registration-form__error-validation">
+                    {errors.additional_lang && 'Это поле обязательно для заполнения'}
+                </div>
             </section>
 
             <section className="registration-form__section">
@@ -136,8 +143,8 @@ const RegistrationForm = () => {
                 <Controller
                     as={
                         <TextareaAutosize
-                            minRows={3}
-                            maxRows={6}
+                            minRows={2}
+                            maxRows={2}
                             placeholder="В кратце опишите Ваши увлечения"
                             className={`registration-form__textarea${errors.about ? '-error' : ''}`}
                             onClick={() => clearErrors('about')}
@@ -152,7 +159,28 @@ const RegistrationForm = () => {
                 </div>
             </section>
 
-            <input type="submit" />
+            <section className="registration-form__section">
+                <label
+                    htmlFor="phone"
+                    className="registration-form__label"
+                >
+                    Контактный телефон (Telegram / WhatsApp)
+                </label>
+                <input
+                    name="phone"
+                    placeholder="Введите Ваши контакты"
+                    className={`registration-form__input${errors.phone ? '-error' : ''}`}
+                    ref={register({ required: true })}
+                    onClick={() => clearErrors('phone')}
+                />
+                <div className="registration-form__error-validation">
+                    {errors.phone && 'Это поле обязательно для заполнения'}
+                </div>
+            </section>
+
+            <button type="submit" className="registration-form__error-button">
+                Отправить
+            </button>
         </form>
     );
 };
